32e08413ae20e8d7d815ee111f76524a2d255cc9
Alexis López Zubieta <azubieta90@gmail.com>
2018-02-23 02:41:54 -0600

    Add missing build script.

---
+++ build-scripts/build-appimage.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+build_scripts_dir=`dirname $0`
+source_dir=`dirname $build_scripts_dir`
+
+DESTDIR=$PWD/appdir make install; find appdir
+
+rsvg-convert -f png -h 256 -w 256 ${source_dir}/res/images/appimage.svg > appdir/appimage.png
+
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage" -O linuxdeployqt-continuous-x86_64.AppImage
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+
+
+unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
+
+./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -bundle-non-qt-libs -appimage
