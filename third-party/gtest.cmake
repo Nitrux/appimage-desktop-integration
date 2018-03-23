663845cdc43143e246e0341e031a4f846223e019
Alexis López Zubieta <azubieta90@gmail.com>
2018-03-23 12:36:06 -0600

    Use libappimage from system if available

--- third-party/gtest.cmake
+++ third-party/gtest.cmake
@@ -1,5 +1,7 @@
 message(STATUS "Downloading and building GTest")
 
+include(ExternalProject)
+
 ExternalProject_Add(gtest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG release-1.8.0
